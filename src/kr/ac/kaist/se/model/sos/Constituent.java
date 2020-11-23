350e107e263dce211be8d4bde79afa1cee66e849
BaekHorang <ymbaek@se.kaist.ac.kr>
2020-11-23 17:19:10 +0900

    JavaDoc updated. - SystemEntity - Constituent

--- src/kr/ac/kaist/se/model/sos/Constituent.java
+++ src/kr/ac/kaist/se/model/sos/Constituent.java
@@ -1,4 +1,15 @@
 package kr.ac.kaist.se.model.sos;
 
-public abstract class Constituent extends SystemEntity {
+import kr.ac.kaist.se.model.intf.DecisionMakeable;
+
+/**
+ * Abstract class to represent a constituent system, called CS.
+ *
+ * According to the M2SoS, a Constituent is one and only object that can belong to an organization.
+ * Since a SystemEntity extends SystemEntity, it is Actionable, Communicatable.
+ * In addition, a Constituent can make its own decisions (i.e., DecisionMakeable).
+ *
+ * @author ymbaek, ehcho, yjshin
+ */
+public abstract class Constituent extends SystemEntity implements DecisionMakeable {
 }
--- src/kr/ac/kaist/se/model/sos/SystemEntity.java
+++ src/kr/ac/kaist/se/model/sos/SystemEntity.java
@@ -1,6 +1,17 @@
 package kr.ac.kaist.se.model.sos;
 
 import kr.ac.kaist.se.model.abst.obj._SimActionableObject_;
+import kr.ac.kaist.se.model.intf.Actionable;
+import kr.ac.kaist.se.model.intf.Communicatable;
 
-public abstract class SystemEntity extends _SimActionableObject_ {
+/**
+ * Abstract class to represent a system (or system-based entity).
+ *
+ * According to the M2SoS, a SystemEntity is a base class to represent a constituent (CS).
+ * A SystemEntity can perform its own actions (i.e., Actionable),
+ * and it can do communication actions (i.e., Communicatable).
+ *
+ * @author ymbaek, ehcho, yjshin
+ */
+public abstract class SystemEntity extends _SimActionableObject_ implements Actionable, Communicatable {
 }
