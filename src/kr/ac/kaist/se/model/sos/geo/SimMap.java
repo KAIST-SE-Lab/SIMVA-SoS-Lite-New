555664b50a625f450e7a87b7e34b89d96fa6feaf
Young-Min Baek <ymbaek@se.kaist.ac.kr>
2021-01-05 00:47:40 +0900

    SimMap updated. - Hashmap for location information added. - MapBuilder included.

--- src/kr/ac/kaist/se/model/sos/geo/SimMap.java
+++ src/kr/ac/kaist/se/model/sos/geo/SimMap.java
@@ -1,5 +1,6 @@
 package kr.ac.kaist.se.model.sos.geo;
 
+import kr.ac.kaist.se.controller.util.MapBuilder;
 import kr.ac.kaist.se.model.abst.geo._SimMap_;
 import kr.ac.kaist.se.model.sos.data.DataVar;
 import kr.ac.kaist.se.model.sos.data.DimensionVar;
@@ -16,13 +17,15 @@ public abstract class SimMap extends _SimMap_ {
 
     /** ArrayLists for MapDimensions and MapInformation */
     //LocDimensions of this map
-    protected ArrayList<DimensionVar> mapDimensions = new ArrayList<>();
+    protected ArrayList<DimensionVar> mapDimVars = new ArrayList<>();
     //LocInformations of this map
-    public ArrayList<DataVar> mapInfos = new ArrayList<>();
+    public ArrayList<DataVar> mapDataVars = new ArrayList<>();
 
     /** Initialization of map */
     //HashMap to store location information
-    protected HashMap<String, ArrayList<DataVar>> mapLocInfos = new HashMap<>();
+    protected HashMap<String, ArrayList<DataVar>> mapLocInfo = new HashMap<>();
+    //MapBuilder to initialize and update a map
+    protected MapBuilder mapBuilder = new MapBuilder();
 
 
     //TODO: Object location hashmap
@@ -34,29 +37,32 @@ public abstract class SimMap extends _SimMap_ {
         this.mapName = mapName;
         //objLocationHashMap = new HashMap<>();
 
-        initMapDimensions();
-        initMapInformation();
+        initMap();
     }
 
     public SimMap(String mapId, String mapName, ArrayList<DimensionVar> mapDimensions) {
         this.mapId = mapId;
         this.mapName = mapName;
-        this.mapDimensions = mapDimensions;
+        this.mapDimVars = mapDimensions;
         //objLocationHashMap = new HashMap<>();
+
+        initMap();
     }
 
     public SimMap(String mapId, String mapName, ArrayList<DimensionVar> mapDimensions, ArrayList<DataVar> mapInfos) {
         this.mapId = mapId;
         this.mapName = mapName;
-        this.mapDimensions = mapDimensions;
-        this.mapInfos = mapInfos;
+        this.mapDimVars = mapDimensions;
+        this.mapDataVars = mapInfos;
+
+        initMap();
     }
 
     protected void initMap(){
         initMapDimensions();
         initMapInformation();
 
-        buildMap("");
+        mapBuilder.updateMapData(mapDimVars, mapDataVars, mapLocInfo, new ArrayList<String>());
     }
 
     //TODO: check return
@@ -66,38 +72,46 @@ public abstract class SimMap extends _SimMap_ {
     protected abstract void initMapInformation();
 
     protected void getAllLocationPoints() {
-        for (DimensionVar dimVar : mapDimensions) {
+        for (DimensionVar dimVar : mapDimVars) {
 
         }
     }
 
     /* Getters & Setters */
 
-    public ArrayList<DimensionVar> getMapDimensions() {
-        return mapDimensions;
+    public ArrayList<DimensionVar> getMapDimVars() {
+        return mapDimVars;
     }
 
-    public void setMapDimensions(ArrayList<DimensionVar> mapDimensions) {
-        this.mapDimensions = mapDimensions;
+    public void setMapDimVars(ArrayList<DimensionVar> mapDimVars) {
+        this.mapDimVars = mapDimVars;
     }
 
     public void addMapDimension(DimensionVar dimensionVar) {
         if (dimensionVar != null) {
-            this.mapDimensions.add(dimensionVar);
+            this.mapDimVars.add(dimensionVar);
         }
     }
 
-    public ArrayList<DataVar> getMapInfos() {
-        return mapInfos;
+    public ArrayList<DataVar> getMapDataVars() {
+        return mapDataVars;
     }
 
-    public void setMapInfos(ArrayList<DataVar> mapInfos) {
-        this.mapInfos = mapInfos;
+    public void setMapDataVars(ArrayList<DataVar> mapDataVars) {
+        this.mapDataVars = mapDataVars;
     }
 
     public void addMapInfo(DataVar infoVar) {
         if (infoVar != null) {
-            this.mapInfos.add(infoVar);
+            this.mapDataVars.add(infoVar);
         }
+    }
+
+    public HashMap<String, ArrayList<DataVar>> getMapLocInfo() {
+        return mapLocInfo;
+    }
+
+    public void setMapLocInfo(HashMap<String, ArrayList<DataVar>> mapLocInfo) {
+        this.mapLocInfo = mapLocInfo;
     }
 }
