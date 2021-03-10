60aba7bf178a369941cd121a4ad92afe40798a88
Young-Min Baek <ymbaek@se.kaist.ac.kr>
2021-03-10 10:06:17 +0900

    [Based on GeoMapBuilder] model::geo - _SimMap_

--- src/kr/ac/kaist/se/model/abst/geo/_SimMap_.java
+++ src/kr/ac/kaist/se/model/abst/geo/_SimMap_.java
@@ -13,4 +13,25 @@ public abstract class _SimMap_ {
 
     protected String mapId;
     protected String mapName;
+
+    public _SimMap_(String mapId, String mapName) {
+        this.mapId = mapId;
+        this.mapName = mapName;
+    }
+
+    public String getMapId() {
+        return mapId;
+    }
+
+    public void setMapId(String mapId) {
+        this.mapId = mapId;
+    }
+
+    public String getMapName() {
+        return mapName;
+    }
+
+    public void setMapName(String mapName) {
+        this.mapName = mapName;
+    }
 }
